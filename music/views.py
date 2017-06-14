05bb49fbfe9bab8253683082412b3e4e034180a5
JacobYRJ <594063939@qq.com>
2017-06-14 14:11:07 +0800

    favoritesuccess

--- music/views.py
+++ music/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.urlresolvers import reverse
 from django.views import generic
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -7,6 +8,7 @@ from .forms import SongForm
 from .models import Song
 
 IMAGE_FILE_TYPES = ['png', 'jpg', 'jpeg']
+song_detail = Song.objects.order_by('name')
 
 
 @login_required
@@ -32,11 +34,18 @@ def add_song(request):
         song.cover = request.FILES['cover']
         song.music = request.FILES['music']
         song.save()
-        return render(request, 'music/detail.html', {'song': song})
+        return redirect(reverse('music:detail', args=[]))
     context = {
         'form': form,
     }
     return render(request, 'music/add_song.html', context)
+
+
+@login_required
+def delete_song(request, song_id):
+    song = Song.objects.get(pk=song_id)
+    song.delete()
+    return redirect(reverse('music:detail', args=[]))
 
 
 @login_required
@@ -48,14 +57,10 @@ def favorite(request, song_id):
         else:
             song.favorite = True
         song.save()
+        context = {
+            'song': song_detail,
+        }
     except(KeyError, Song.DoesNotExist):
-        return JsonResponse({'success': False})
+        return redirect(reverse('music:detail', args=[]))
     else:
-        return JsonResponse({'success': True})
-
-
-@login_required
-def delete_song(request, song_id):
-    song = Song.objects.get(pk=song_id)
-    song.delete()
-    return render(request, 'music/detail.html', {'song': song})
+        return redirect(reverse('music:detail', args=[]))
